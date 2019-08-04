31b2d140d591fa34a7bbc0afa830809d2164f379
Jahnavi <muddujahnavi860@gmail.com>
2019-08-04 15:25:44 +0530

    Availability layout needs to be improved.

--- app/src/main/java/com/vineethn7/labor/AvailabilityActivity.java
+++ app/src/main/java/com/vineethn7/labor/AvailabilityActivity.java
@@ -11,6 +11,8 @@ import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
+//Availability layout needs to be updated
+
 public class AvailabilityActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
     Switch swAvailable;
     Button btnAccept,btnReject;
