476d2b80f0eda7fbe3907703e819423a4ae6cfb5
Kiran <sai8kiran99@gmail.com>
2019-09-20 10:35:05 +0530

    Login through OTP working

--- app/src/main/java/com/mvsrecproject/labor/LoginActivity.java
+++ app/src/main/java/com/mvsrecproject/labor/LoginActivity.java
@@ -1,17 +1,34 @@
 package com.mvsrecproject.labor;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
 import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
     EditText etPhone,etOTP;
-    Button btnLogin;
+    Button btnLogin,btnGetOTP;
+    FirebaseAuth mAuth;
+    private String OTPCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,23 +36,94 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         etPhone = findViewById(R.id.etPhone);
         etOTP = findViewById(R.id.etOTP);
+        btnGetOTP = findViewById(R.id.btngetotp);
         btnLogin = findViewById(R.id.btnLogin);
+        mAuth = FirebaseAuth.getInstance();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+        btnGetOTP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(etPhone.getText().toString().isEmpty() || etPhone.getText().toString().trim().length() != 10)
-                {
-                    Toast.makeText(LoginActivity.this, "Please enter valid phone number!", Toast.LENGTH_SHORT).show();
+                String phonenum = etPhone.getText().toString().trim();
+                if(phonenum.length()!=10){
+                    etPhone.setError("Enter Valid phone number");
+                    etPhone.requestFocus();
+                    return;
                 }
-                else
-                {
-                    BigInteger phoneno = new BigInteger(etPhone.getText().toString().trim());
-                    Intent intent= new Intent(LoginActivity.this, com.mvsrecproject.labor.SelectorActivity.class);
-                    intent.putExtra("PhoneNum",phoneno);
+                sendOTP(phonenum);
+            }
+        });
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            String phonenum = etPhone.getText().toString().trim();
+            @Override
+            public void onClick(View v) {
+                if(phonenum.length()!=10 || etOTP.getText().toString().trim().isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Enter all fields!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String otp = etOTP.getText().toString().trim();
+                verifyOTP(otp);
+            }
+        });
+    }
+
+    //Method that will send OTP code.
+    private void sendOTP(String phno){
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91"+phno,
+                30,
+                TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                mCallBack
+                );
+    }
+
+    private void verifyOTP(String code){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(OTPCode,code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this,SelectorActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.putExtra("PhoneNum",etPhone.getText().toString().trim());
                     startActivity(intent);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            OTPCode = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            String otp = phoneAuthCredential.getSmsCode();
+            if(otp!=null){
+                verifyOTP(otp);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    };
 }
+
