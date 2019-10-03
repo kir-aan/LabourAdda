691f8db5b39793a064f1f39c33a81dc77570c720
Kiran <sai8kiran99@gmail.com>
2019-10-03 16:10:30 +0530

    Removed getter and setter for "number"

--- app/src/main/java/com/mvsrecproject/labor/Contractor.java
+++ app/src/main/java/com/mvsrecproject/labor/Contractor.java
@@ -29,13 +29,13 @@ public class Contractor {
         this.name = name;
     }
 
-    public long getNumber() {
-        return number;
-    }
+//    public long getNumber() {
+//        return number;
+//    }
 
-    public void setNumber(long number) {
-        this.number = number;
-    }
+//    public void setNumber(long number) {
+//        this.number = number;
+//    }
 
     public int getContractorAge() {
         return ContractorAge;
