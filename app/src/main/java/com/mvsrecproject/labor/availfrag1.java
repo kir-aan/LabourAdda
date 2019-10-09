642fef4f2dc97e40e4ee3e2c7d9effae87fa69e1
Kiran <sai8kiran99@gmail.com>
2019-10-09 12:13:27 +0530

    Added code from<Vineeth>

--- app/src/main/java/com/mvsrecproject/labor/availfrag1.java
+++ app/src/main/java/com/mvsrecproject/labor/availfrag1.java
@@ -12,17 +12,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class availfrag1 extends Fragment {
-
-
+    FirebaseUser user;
+    DatabaseReference availableLabors;
+    private Boolean status;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_availfrag1,container,false);
-
-
+        availableLabors= FirebaseDatabase.getInstance().getReference("Labors");
+        user =FirebaseAuth.getInstance().getCurrentUser();
 
         final Switch Avail =(Switch)view.findViewById(R.id.Avail);
         Avail.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -34,25 +41,30 @@ public class availfrag1 extends Fragment {
                         android.R.animator.fade_out);
                 if (isChecked)
                 {
-
+                    status=true;
                     // FragmentTransaction fr = getFragmentManager().beginTransaction();
                     fr.add(R.id.fragment_container2, new availfrag2());
                     fr.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
                     fr.commit();
+
 
                 }
 
 
                 else
                 {
-                  //  fr.remove(new availfrag2());
+                    status=false;
+                    //  fr.remove(new availfrag2());
                     //fr.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
                     fr.replace(R.id.fragment_container2,new BlankFragment());
-                   // fr.addToBackStack(null);
+                    // fr.addToBackStack(null);
                     fr.commit();
 
                     //   fr.hide(R.id.fragment_container2, new availfrag2())
                 }
+
+                availableLabors.child(user.getUid()).child("availabilityStatus").setValue(status);
+
             }
 
         });
