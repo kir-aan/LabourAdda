879a0c0ba5ecbc8ba2a3ee728bb6fb2d0590de5b
Kiran <sai8kiran99@gmail.com>
2019-10-12 11:56:30 +0530

    Created New Java Class: LaborAdapter (Required for labors_selection activity)

---
+++ app/src/main/java/com/mvsrecproject/labor/LaborAdapter.java
@@ -0,0 +1,53 @@
+package com.mvsrecproject.labor;
+
+import java.io.Serializable;
+
+public class LaborAdapter implements Serializable {
+
+    private String laborName;
+    private int laborAge;
+
+    private boolean active;
+
+    public LaborAdapter(String laborName, int laborAge)  {
+        this.laborName= laborName;
+        this.laborAge = laborAge;
+        this.active= false;
+    }
+
+    public LaborAdapter(String laborName, int laborAge, boolean active)  {
+        this.laborName= laborName;
+        this.laborAge = laborAge;
+        this.active= active;
+    }
+
+    public int getlaborAge() {
+        return laborAge;
+    }
+
+    public void setLaborAge(int laborAge) {
+        this.laborAge = laborAge;
+    }
+
+    public String getLaborName() {
+        return laborName;
+    }
+
+    public void setLaborName(String laborName) {
+        this.laborName = laborName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return this.laborName +" ("+ this.laborAge+")";
+    }
+
+}
