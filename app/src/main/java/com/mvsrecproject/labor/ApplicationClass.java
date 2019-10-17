2ce33cfe10bb65b8227849f57d31b4c251b51eef
Kiran <sai8kiran99@gmail.com>
2019-10-17 13:10:21 +0530

    Retrieving data from Requests Node(From FB_RealtimeDB)

--- app/src/main/java/com/mvsrecproject/labor/ApplicationClass.java
+++ app/src/main/java/com/mvsrecproject/labor/ApplicationClass.java
@@ -2,31 +2,75 @@ package com.mvsrecproject.labor;
 
 import android.app.Application;
 
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class ApplicationClass extends Application {
 
     public static ArrayList<Contractor> contractors;
 
+    FirebaseUser labor;
+    DatabaseReference laborRootNode,requestRootNode;
+
+
     @Override
     public void onCreate() {
         super.onCreate();
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null) {
+            contractors=new ArrayList<Contractor>();
+            labor = FirebaseAuth.getInstance().getCurrentUser();
+            laborRootNode = FirebaseDatabase.getInstance()
+                    .getReference("Labors").child(labor.getUid()).child("labourSkill");
+            requestRootNode = FirebaseDatabase.getInstance().getReference("Requests");
 
-        contractors=new ArrayList<Contractor>();
-        contractors.add(new Contractor("Contractor1",9087654321L));
-        contractors.add(new Contractor("Contractor2",8907632189L));
-        contractors.add(new Contractor("Contractor3",7854321908L));
-        contractors.add(new Contractor("Contractor4",7854321908L));
-        contractors.add(new Contractor("Contractor5",1234567800L));
-        contractors.add(new Contractor("Contractor6",1234567800L));
-        contractors.add(new Contractor("Contractor7",1234567800L));
-        contractors.add(new Contractor("Contractor8",1234567800L));
-        contractors.add(new Contractor("Contractor9",1234567800L));
-        contractors.add(new Contractor("Contractor10",1234567800L));
-        contractors.add(new Contractor("Contractor11",1234567800L));
-        contractors.add(new Contractor("Contractor12",1234567800L));
-        contractors.add(new Contractor("Contractor13",1234567800L));
-        contractors.add(new Contractor("Contractor14",1234567800L));
+            laborRootNode.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
+                    dataSnapshot.getValue();
+                    requestRootNode.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot1) {
+                            for (DataSnapshot ds1 : dataSnapshot1.getChildren()) {
+                                if (ds1.hasChild(dataSnapshot.getValue().toString())) {
+                                    if (ds1.child(dataSnapshot.getValue().toString()).hasChild(labor.getUid())) {
+                                        System.out.println("Found labour in requests");
+                                        System.out.println(ds1.child("contName").getValue());
+                                        System.out.println(ds1.child("phoneNum").getValue());
+                                        String contName = ds1.child("contName").getValue().toString();
+                                        String phoneNum = ds1.child("phoneNum").getValue().toString();
+                                        contractors.add(new Contractor(contName,phoneNum));
+                                    } else {
+                                        System.out.println("labour not found in requests");
+                                    }
+                                } else {
+                                    System.out.println("L not Found");
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
 
     }
 }
